a3eb0bf03269a3ff5cb2d162c4d4527c8761ce30
julietezekwe <julietezekwe@gmail.com>
2018-08-10 23:45:48 +0100

    adding  view dependencies, creating the routes and views

--- app.js
+++ app.js
@@ -1,6 +1,20 @@
 import express from "express";
+import path from "path";
+
 
 const app = express();
+//creating view and public folders
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+app.use(express.static(path.join(__dirname, 'public')));
+
+
+
+
+app.get('/', (req, res)=>{
+ 
+  res.render('index');
+  });
 
 const port = parseInt(process.env.PORT, 10) || 8000;
 
